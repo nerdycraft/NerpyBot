f36ffe9f5bec3c671c9fabc062705dd7d11fd21d
Rikshy <r.wesenberg@gmail.com>
2021-01-29 15:52:48 +0100

    moved timed loop to cog

--- NerdyPy/NerdyPy.py
+++ NerdyPy/NerdyPy.py
@@ -18,7 +18,6 @@ from discord.ext import commands
 from utils.reminder import Reminder
 from utils.database import create_all, session_scope
 from utils.errors import NerpyException
-from utils.timed import Timed
 
 
 class NerpyBot(commands.Bot):
@@ -41,7 +40,6 @@ class NerpyBot(commands.Bot):
 
         self.audio = Audio(self)
         self.reminder = Reminder(self)
-        self.timed = Timed(self)
         self.last_cmd_cache = {}
 
         create_all()
@@ -124,7 +122,6 @@ class NerpyBot(commands.Bot):
         self.restart = False
         await self.audio.rip_loop()
         await self.reminder.rip_loop()
-        await self.timed.rip_loop()
         await self.logout()
 
     def _import_modules(self):
