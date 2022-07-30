43b7064fc91f28de0f15c8f38fe61ea28591faf1
karaktaka <karaktaka@users.noreply.github.com>
2022-07-30 23:11:51 +0200

    checks
    check if user is connected to a voice channel

--- NerdyPy/utils/checks.py
+++ NerdyPy/utils/checks.py
@@ -15,3 +15,13 @@ async def is_botmod(ctx):
             if role.name == ctx.bot.moderator_role:
                 return True
     return False
+
+
+async def is_connected_to_voice(ctx):
+    if ctx.author.voice is None:
+        await ctx.author.send("I don't know where you are. Please connect to a voice channel.")
+        return False
+    if not ctx.author.voice.channel.permissions_for(ctx.guild.me).connect:
+        await ctx.author.send("I'm not allowed to join you in your current channel.")
+        return False
+    return True
