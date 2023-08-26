a2c3e74d5fe9dad350ce55d0696029354d5438e3
karaktaka <karaktaka@users.noreply.github.com>
2023-08-26 11:26:27 +0200

    moved autodeleter to admin module

--- NerdyPy/modules/admin.py
+++ NerdyPy/modules/admin.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime, timedelta, timezone, time
-from typing import Optional
+from typing import Optional, Union
 
+import discord
 import humanize
 import pytimeparse
 from discord.app_commands import checks
 from discord.ext import tasks
-from discord.ext.commands import GroupCog, hybrid_command, group
+from discord.ext.commands import Cog, hybrid_command, hybrid_group, group
 
+from models.AutoDelete import AutoDelete
 from models.GuildPrefix import GuildPrefix
 from models.RoleChecker import RoleChecker
 
@@ -18,7 +20,7 @@ loop_run_time = time(hour=12, minute=30, tzinfo=utc)
 
 
 @checks.has_permissions(administrator=True)
-class Admin(GroupCog):
+class Admin(Cog):
     """cog for administrative usage"""
 
     def __init__(self, bot):
@@ -26,13 +28,15 @@ class Admin(GroupCog):
 
         self.bot = bot
         self.settings = {}
-        self._role_checker.start()
+        self._rolechecker.start()
+        self._autodeleter.start()
 
     def cog_unload(self):
-        self._role_checker.cancel()
+        self._rolechecker.cancel()
+        self._autodeleter.cancel()
 
     @tasks.loop(time=loop_run_time)
-    async def _role_checker(self):
+    async def _rolechecker(self):
         with self.bot.session_scope() as session:
             for guild in self.bot.guilds:
                 configuration = RoleChecker.get(guild.id, session)
@@ -59,10 +63,27 @@ class Admin(GroupCog):
                                         f"Please choose a role until {humanize.naturaldate(kick_after)}."
                                     )
 
-    @_role_checker.before_loop
-    async def _role_checker_before_loop(self):
-        self.bot.log.info("Waiting for Bot to be ready...")
-        await self.bot.wait_until_ready()
+    @tasks.loop(minutes=5)
+    async def _autodeleter(self):
+        with self.bot.session_scope() as session:
+            for guild in self.bot.guilds:
+                configurations = AutoDelete.get(guild.id, session)
+
+                for configuration in configurations:
+                    if configuration.DeleteOlderThan is None:
+                        list_before = None
+                    else:
+                        list_before = datetime.utcnow() - timedelta(seconds=configuration.DeleteOlderThan)
+                        list_before = list_before.replace(tzinfo=timezone.utc)
+                    channel = guild.get_channel(configuration.ChannelId)
+                    messages = [message async for message in channel.history(before=list_before, oldest_first=True)]
+
+                    while len(messages) > configuration.KeepMessages:
+                        message = messages.pop(0)
+                        if not configuration.DeletePinnedMessage and message.pinned:
+                            continue
+                        self.bot.log.info(f"Delete message {message.id}, created at {message.created_at}.")
+                        await message.delete()
 
     @hybrid_command()
     async def rolechecker(self, ctx, enable: bool, kick_after: str, kick_reminder_message: Optional[str]):
@@ -71,6 +92,9 @@ class Admin(GroupCog):
             configuration = RoleChecker.get(ctx.guild.id, session)
             if kick_after is not None:
                 kick_time = pytimeparse.parse(kick_after)
+                if kick_time is None:
+                    await ctx.send("Only timespans up until weeks are allowed. Do not use months or years.")
+                    return
             else:
                 await ctx.send("You need to specify when I should kick someone!")
                 return
@@ -88,6 +112,94 @@ class Admin(GroupCog):
                 session.add(rolechecker)
 
         await ctx.send("RoleChecker configured for this server.")
+
+    @hybrid_group()
+    async def autodeleter(self, ctx):
+        """Manage autodeletion per channel [bot-moderator]"""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @autodeleter.command(name="create")
+    async def create_autodeleter(
+        self,
+        ctx,
+        *,
+        channel: discord.TextChannel,
+        delete_older_than: Optional[Union[str | None]],
+        keep_messages: Optional[Union[int | None]],
+        delete_pinned_message: bool,
+    ):
+        channel_id = channel.id
+        channel_name = channel.name
+
+        with self.bot.session_scope() as session:
+            configuration = AutoDelete.get_by_channel(ctx.guild.id, channel_id, session)
+            if configuration is not None:
+                await ctx.send(
+                    "This Channel is already configured for AutoDelete."
+                    "Please edit or delete the existing configuration."
+                )
+            else:
+                if ctx.guild.get_channel(channel_id) is not None:
+                    if delete_older_than is None:
+                        delete = delete_older_than
+                    else:
+                        delete = pytimeparse.parse(delete_older_than)
+                        if delete is None:
+                            await ctx.send("Only timespans up until weeks are allowed. Do not use months or years.")
+                            return
+
+                    deleter = AutoDelete(
+                        GuildId=ctx.guild.id,
+                        ChannelId=channel_id,
+                        KeepMessages=keep_messages,
+                        DeleteOlderThan=delete,
+                        DeletePinnedMessage=delete_pinned_message,
+                    )
+                    session.add(deleter)
+
+        await ctx.send(f'AutoDeleter configured for channel "{channel_name}".')
+
+    @autodeleter.command(name="delete")
+    async def delete_autodeleter(self, ctx, *, channel: discord.TextChannel):
+        channel_id = channel.id
+        channel_name = channel.name
+
+        with self.bot.session_scope() as session:
+            configuration = AutoDelete.get_by_channel(ctx.guild.id, channel_id, session)
+            if configuration is not None:
+                AutoDelete.delete(ctx.guild.id, channel_id, session)
+                await ctx.send(f'Deleted configuration for channel "{channel_name}".')
+            else:
+                await ctx.send(f'No configuration for channel "{channel_name}" found!')
+
+    @autodeleter.command(name="edit")
+    async def modify_autodeleter(
+        self,
+        ctx,
+        *,
+        channel: discord.TextChannel,
+        delete_older_than: Optional[Union[str | None]],
+        keep_messages: Optional[Union[int | None]],
+        delete_pinned_message: bool,
+    ):
+        channel_id = channel.id
+        channel_name = channel.name
+
+        with self.bot.session_scope() as session:
+            configuration = AutoDelete.get_by_channel(ctx.guild.id, channel_id, session)
+            if configuration is not None:
+                if delete_older_than is None:
+                    configuration.DeleteOlderThan = delete_older_than
+                else:
+                    delete_in_seconds = pytimeparse.parse(delete_older_than)
+                    configuration.DeleteOlderThan = delete_in_seconds
+                configuration.KeepMessages = keep_messages
+                configuration.DeletePinnedMessage = delete_pinned_message
+
+                await ctx.send(f'Updated configuration for channel "{channel_name}".')
+            else:
+                await ctx.send(f'Configuration for channel "{channel_name}" does not exist. Please create one first.')
 
     @group()
     async def prefix(self, ctx):
@@ -135,6 +247,16 @@ class Admin(GroupCog):
     async def _bot_leave_channel(self, ctx):
         """bot leaves the channel [bot-moderator]"""
         await self.bot.audio.leave(ctx.guild.id)
+
+    @_rolechecker.before_loop
+    async def _role_checker_before_loop(self):
+        self.bot.log.info("Rolechecker: Waiting for Bot to be ready...")
+        await self.bot.wait_until_ready()
+
+    @_autodeleter.before_loop
+    async def _autodeleter_before_loop(self):
+        self.bot.log.info("AutoDeleter: Waiting for Bot to be ready...")
+        await self.bot.wait_until_ready()
 
 
 async def setup(bot):
