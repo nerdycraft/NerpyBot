764352293ca775639d549ff6e7f742997225e793
Dennis <karaktaka@users.noreply.github.com>
2024-04-18 23:43:45 +0200

    F database models (#105)
    * Fix Models
    
    String needs a length for mariadb
    
    * Update __init__.py

--- NerdyPy/models/__init__.py
+++ NerdyPy/models/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
+import models.AutoDelete
 import models.AutoKicker
 import models.GuildPrefix
 import models.RaidEncounter
 import models.RaidEncounterRole
+import models.RaidEvent
 import models.RaidTemplate
 import models.Reminder
 import models.Tag
