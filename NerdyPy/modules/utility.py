b8db2de4c7346eb533b330f1897d03c9d6d73e00
Dennis <karaktaka@users.noreply.github.com>
2025-07-31 19:51:37 +0200

    use openweather lib

--- NerdyPy/modules/utility.py
+++ NerdyPy/modules/utility.py
@@ -2,9 +2,10 @@
 
 from datetime import UTC, datetime
 
-from aiohttp import ClientSession
-from discord import Embed, app_commands
-from discord.ext.commands import Cog, Context, bot_has_permissions, hybrid_command
+from discord import app_commands, Embed
+from discord.ext.commands import Cog, hybrid_command, bot_has_permissions, Context
+from openweather.weather import OpenWeather
+
 from utils import format as fmt
 from utils.errors import NerpyException
 from utils.helpers import send_hidden_message
@@ -16,7 +17,7 @@ class Utility(Cog):
         bot.log.info(f"loaded {__name__}")
 
         self.bot = bot
-        self.weather_api_key = self.bot.config.get("utility", "openweather")
+        self.weather_api = OpenWeather(self.bot.config["utility"]["openweather"])
 
     @hybrid_command(name="ping", hidden=True)
     @bot_has_permissions(send_messages=True)
@@ -37,75 +38,62 @@ class Utility(Cog):
     @bot_has_permissions(embed_links=True)
     async def _get_weather(self, ctx: Context, *, query: str):
         """outputs weather information"""
-        location_url = f"http://api.openweathermap.org/geo/1.0/direct?q={query}&appid={self.weather_api_key}"
+        try:
+            async with ctx.typing():
+                for umlaut in ["ä", "ö", "ü"]:
+                    if umlaut in query:
+                        await send_hidden_message(ctx, "Please use english names only!")
+                        return
 
-        for umlaut in ["ä", "ö", "ü"]:
-            if umlaut in query:
-                await send_hidden_message(ctx, "Please use english names only!")
-                return
+                weather = self.weather_api.get_weather(city=query)
 
-        async with ClientSession() as session:
-            async with session.get(location_url) as response:
-                if response.status != 200:
-                    err = f"The api-webserver responded with a code: {response.status} - {response.reason}"
-                    raise NerpyException(err)
-                location = await response.json()
+                conditions = []
+                for w in weather.get("weather", dict()):
+                    conditions.append(w.get("main"))
 
-        if location is None:
-            return
+                sunrise = datetime.fromtimestamp(int(weather.get("sys", dict()).get("sunrise"))).strftime("%H:%M")
+                sunset = datetime.fromtimestamp(int(weather.get("sys", dict()).get("sunset"))).strftime("%H:%M")
 
-        lat = location[0].get("lat")
-        lon = location[0].get("lon")
-        weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={self.weather_api_key}&units=metric"
+                emb = Embed()
+                emb.add_field(
+                    name=f':earth_africa: {fmt.bold("location")}',
+                    value=f"""[{weather.get("name")},
+                    {weather.get("sys", dict()).get("country")}](https://openweathermap.org/city/{weather.get("id")})""",
+                )
+                temp = self.weather_api.convert_temperature(weather['main']['temp'])
+                emb.add_field(
+                    name=f':thermometer: {fmt.bold("temperature")}',
+                    value=f"{temp:.2f}°C",
+                )
+                emb.add_field(
+                    name=f':cloud: {fmt.bold("condition")}',
+                    value=str.join(", ", conditions),
+                )
+                emb.add_field(
+                    name=f':sweat_drops: {fmt.bold("humidity")}',
+                    value=f"{weather['main']['humidity']}%",
+                )
+                emb.add_field(
+                    name=f':wind_chime: {fmt.bold("wind")}',
+                    value=f"{weather['wind']['speed']} m/s",
+                )
+                temp_min = self.weather_api.convert_temperature(weather['main']['temp_min'])
+                temp_max = self.weather_api.convert_temperature(weather['main']['temp_max'])
+                emb.add_field(
+                    name=f'🔆 {fmt.bold("min-max")}',
+                    value=f"{temp_min:.2f}°C - {temp_max:.2f}°C",
+                )
+                emb.add_field(name=f':city_sunrise: {fmt.bold("sunrise")}', value=f"{sunrise} UTC")
+                emb.add_field(name=f':city_sunset:  {fmt.bold("sunset")}', value=f"{sunset} UTC")
+                emb.set_footer(
+                    text="Powered by openweathermap.org",
+                    icon_url=f'http://openweathermap.org/img/w/{weather.get("weather", list())[0].get("icon")}.png',
+                )
 
-        async with ClientSession() as session:
-            async with session.get(weather_url) as response:
-                if response.status != 200:
-                    err = f"The api-webserver responded with a code: {response.status} - {response.reason}"
-                    raise NerpyException(err)
-                weather = await response.json()
-
-        conditions = []
-        for w in weather.get("weather", dict()):
-            conditions.append(w.get("main"))
-
-        sunrise = datetime.fromtimestamp(int(weather.get("sys", dict()).get("sunrise"))).strftime("%H:%M")
-        sunset = datetime.fromtimestamp(int(weather.get("sys", dict()).get("sunset"))).strftime("%H:%M")
-
-        emb = Embed()
-        emb.add_field(
-            name=f":earth_africa: {fmt.bold('location')}",
-            value=f"""[{weather.get("name")},
-            {weather.get("sys", dict()).get("country")}](https://openweathermap.org/city/{weather.get("id")})""",
-        )
-        emb.add_field(
-            name=f":thermometer: {fmt.bold('temperature')}",
-            value=f"{weather['main']['temp']}°C",
-        )
-        emb.add_field(
-            name=f":cloud: {fmt.bold('condition')}",
-            value=str.join(", ", conditions),
-        )
-        emb.add_field(
-            name=f":sweat_drops: {fmt.bold('humidity')}",
-            value=f"{weather['main']['humidity']}%",
-        )
-        emb.add_field(
-            name=f":wind_chime: {fmt.bold('wind')}",
-            value=f"{weather['wind']['speed']} m/s",
-        )
-        emb.add_field(
-            name=f"🔆 {fmt.bold('min-max')}",
-            value=f"{weather['main']['temp_min']}°C - {weather['main']['temp_max']}°C",
-        )
-        emb.add_field(name=f":city_sunrise: {fmt.bold('sunrise')}", value=f"{sunrise} UTC")
-        emb.add_field(name=f":city_sunset:  {fmt.bold('sunset')}", value=f"{sunset} UTC")
-        emb.set_footer(
-            text="Powered by openweathermap.org",
-            icon_url=f"http://openweathermap.org/img/w/{weather.get('weather', list())[0].get('icon')}.png",
-        )
-
-        await ctx.send(embed=emb)
+                await ctx.send(embed=emb)
+        except Exception as ex:
+            self.bot.log.error(f"Error while fetching weather: {ex}")
+            await send_hidden_message(ctx, "An error occurred while fetching the weather information.")
 
 
 async def setup(bot):
